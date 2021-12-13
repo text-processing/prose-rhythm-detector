c5bc0448920e7e6390e8c9790bff47ae58104967
text-processing <52315161+text-processing@users.noreply.github.com>
2021-12-13 11:06:42 +0300

    Update verification_ml.py

--- app/classification_utils/verification/verification_ml.py
+++ app/classification_utils/verification/verification_ml.py
@@ -19,49 +19,58 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.model_selection import KFold
 from sklearn.metrics import precision_score, recall_score
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_validate
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
-csv_filenames = ["es_rhythm_no_sums.csv", "fr_rhythm_no_sums.csv", "en_rhythm_no_sums.csv", "ru_rhythm_no_sums.csv"]
-result_file = open('RandomForest_verification.txt', "w")
+
+lang = "ru"
+csv_filenames = [
+    #"tmp.csv"
+    "ru_science_articles_rhythm_no_sums.csv"
+    #lang + "_char.csv",
+    #lang + "_word.csv", lang + "_rhythm.csv",
+                 ##lang + "_char_and_word.csv", lang + "_char_and_rhythm.csv", 
+                 ##lang + "_word_and_rhythm.csv",
+                 #lang + "_all_features.csv"
+                 ]
+result_file = open('AdaBoost_verification_with_pca_'+ lang+'.txt', "w")
 
 
-def verify_once():
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=100)
-    # classifier = RandomForestClassifier(n_estimators=50, max_depth=3, random_state=100)
+def getFeatureTypeByFilename(filename):
+    if "_all_features" in filename:
+        return "7-All"
+    elif "_word_and_rhythm" in filename:
+        return "6-W + Rh"
+    elif "_char_and_rhythm" in filename:
+        return "5-Ch + Rh"
+    elif "_char_and_word" in filename:
+        return "4-Ch + W"
+    elif "_rhythm" in filename:
+        return "3-Rh"
+    elif "_word" in filename:
+        return "2-W"
+    else:
+        return "1-Ch"
+
+
+def cross_val(filename):
+    #classifier = RandomForestClassifier(n_estimators=50, max_depth=3, random_state=100)
     classifier = AdaBoostClassifier(n_estimators=50, random_state=100)
-    result_file.write(str(classifier) + '\n')
-    model = classifier.fit(X_train, y_train)
-    predictions = model.predict(X_test)
-    precision = precision_score(y_test, predictions, average='macro') * 100
-    precisions.append(precision)
-    result_file.write(str(round(precision, 1)) + ' & ')
-    recall = recall_score(y_test, predictions, average='macro') * 100
-    recalls.append(recall)
-    result_file.write(str(round(recall, 1)) + ' & ')
-    f_measure = 2 * precision * recall / (precision + recall)
-    result_file.write(str(round(f_measure, 1)) + '\n')
-
-
-def cross_validate():
-    result_file.write("Cross-validation\n")
-    classifier = RandomForestClassifier(n_estimators=50, max_depth=3, random_state=100)
-    #classifier = AdaBoostClassifier(n_estimators=50, random_state=100)
-    results = cross_val_score(classifier, X, y, cv=KFold(n_splits=5, shuffle=True), scoring='precision_macro')
-    precision = results.mean() * 100
-    precision_variability = results.std() * 100
-    results = cross_val_score(classifier, X, y, cv=KFold(n_splits=5, shuffle=True), scoring='recall_macro')
-    recall = results.mean() * 100
-    recall_variability = results.std() * 100
-    results = cross_val_score(classifier, X, y, cv=KFold(n_splits=5, shuffle=True), scoring='f1_macro')
-    f_measure = results.mean() * 100
-    f_measure_variability = results.std() * 100
+    results = cross_validate(classifier, X, y, cv=KFold(n_splits=5, shuffle=True), scoring=['precision_macro', 'recall_macro', 'f1_macro'])
+    precision = results['test_precision_macro'].mean() * 100
+    precision_variability = results['test_precision_macro'].std() * 100
+    recall = results['test_recall_macro'].mean() * 100
+    recall_variability = results['test_recall_macro'].std() * 100
+    f_measure = results['test_f1_macro'].mean() * 100
+    f_measure_variability = results['test_f1_macro'].std() * 100
     precisions.append(precision)
     recalls.append(recall)
-    result_file.write(author + ' & ' + str(round(precision, 1)) + ' & ' + str(round(precision_variability, 1)) +
+    result_file.write(author + ' & ' + getFeatureTypeByFilename(filename) + ' & ' + str(round(precision, 1)) + ' & ' + str(round(precision_variability, 1)) +
                       ' & ' + str(round(recall, 1)) + ' & ' + str(round(recall_variability, 1)) +
-                      ' & ' + str(round(f_measure, 1)) + ' & ' + str(round(f_measure_variability, 1)) + '\n')
+                      ' & ' + str(round(f_measure, 1)) + ' & ' + str(round(f_measure_variability, 1)) + ' \\\\ \n')
 
 
 for filename in csv_filenames:
@@ -72,20 +81,35 @@ for filename in csv_filenames:
     unique_classes = set(classes_labels)
     result_file.write(str(unique_classes) + '\n')
     
-    df = (df-df.mean())/df.std()
+    #df = df.drop(columns=['B', 'C'])    
+    
+    #df = (df-df.mean())/df.std()
+    df = df.dropna()
+    #print(df)
     X = df.to_numpy()
+    #print(X)
+    #quit()
+    
+    #X = StandardScaler().fit_transform(df)
+    #if filename == lang + "_rhythm.csv":
+        #n_components = len(df.columns) - 5
+    #elif filename == lang + "_all_features.csv":
+        #n_components=len(df.columns) - 25
+    #else:
+        #n_components=len(df.columns) - 10
+    #pca = PCA(n_components=n_components)
+    #X = pca.fit_transform(X)
 
     precisions = []
     recalls = []
     for author in unique_classes:
         print(author)
-        result_file.write('Verify ' + author + '\n')
+        #result_file.write('Verify ' + author + '\n')
         y = [0 if label == author else 1 for label in classes_labels]
-        result_file.write('Number of fragments: ' + str(len(y) - sum(y)) + '\n')
-        #verify_once()
-        cross_validate()
-        result_file.write('\n')
-    result_file.write('Average metrics\n')
+        #result_file.write('Number of fragments: ' + str(len(y) - sum(y)) + '\n')
+        cross_val(filename)
+        #result_file.write('\n')
+    result_file.write('Average metrics\n' + getFeatureTypeByFilename(filename) + ' & ')
     mean_precision = sum(precisions) / len(precisions)
     result_file.write(str(round(mean_precision, 1)) + ' & ')
     mean_recall = sum(recalls) / len(recalls)
